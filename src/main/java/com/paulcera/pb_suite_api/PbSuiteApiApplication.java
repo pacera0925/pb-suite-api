21c36afa23217744c470f2313573acff84cb2e6b
Paul Cera <pacera0925@gmail.com>
2024-08-23 00:41:54 +0800

    Initial commit

---
+++ src/main/java/com/paulcera/pb_suite_api/PbSuiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.paulcera.pb_suite_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbSuiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbSuiteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulcera/pb_suite_api/PbSuiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulcera.pb_suite_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbSuiteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
